40403f4370febf053a5b772782e3d117d330bc6a
Abdul Rafey <abdulrafey0824@gmail.com>
2024-11-27 21:16:28 +0500

    Developed affiliate fashboard table and integrated api

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import VideoSubtitle from "./components/Tools/VideoSubtitle";
 import Test from "./components/Tools/Test";
+import AffiliateDashboard from "./pages/AffiliateDashboard";
 export default function App() {
   Aos.init();
   return (
@@ -12,6 +13,11 @@ export default function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/subtitle" element={<VideoSubtitle />} />
+        <Route
+          exact
+          path="/affiliate-dashboard"
+          element={<AffiliateDashboard />}
+        />
       </Routes>
     </BrowserRouter>
   );
